6539d06ef8ca60c9b5aa9457743561308980c0e7
Matthew Strimas-Mackey <mes335@cornell.edu>
2025-12-17 11:40:02 +0000

    version 0.9.0

--- inst/doc/auk.R
+++ inst/doc/auk.R
@@ -14,14 +14,14 @@ suppressPackageStartupMessages(library(dplyr))
 # input_file <- system.file("extdata/ebd-sample.txt", package = "auk")
 # # output text file
 # output_file <- "ebd_filtered_grja.txt"
-# ebird_data <- input_file %>%
+# ebird_data <- input_file |>
 #   # 1. reference file
-#   auk_ebd() %>%
+#   auk_ebd() |>
 #   # 2. define filters
-#   auk_species(species = "Canada Jay") %>%
-#   auk_country(country = "Canada") %>%
+#   auk_species(species = "Canada Jay") |>
+#   auk_country(country = "Canada") |>
 #   # 3. run filtering
-#   auk_filter(file = output_file) %>%
+#   auk_filter(file = output_file) |>
 #   # 4. read text file into r data frame
 #   read_ebd()
 
@@ -46,67 +46,67 @@ suppressPackageStartupMessages(library(dplyr))
 # system.file("extdata/zerofill-ex_sampling.txt", package = "auk")
 
 ## ----auk-ebd------------------------------------------------------------------
-ebd <- system.file("extdata/ebd-sample.txt", package = "auk") %>% 
+ebd <- system.file("extdata/ebd-sample.txt", package = "auk") |>
   auk_ebd()
 ebd
 
 ## ----auk-filter---------------------------------------------------------------
-ebd_filters <- ebd %>% 
+ebd_filters <- ebd |>
   # species: common and scientific names can be mixed
-  auk_species(species = c("Canada Jay", "Cyanocitta cristata")) %>%
+  auk_species(species = c("Canada Jay", "Cyanocitta cristata")) |>
   # country: codes and names can be mixed; case insensitive
-  auk_country(country = c("US", "Canada", "mexico")) %>%
+  auk_country(country = c("US", "Canada", "mexico")) |>
   # bbox: long and lat in decimal degrees
   # formatted as `c(lng_min, lat_min, lng_max, lat_max)`
-  auk_bbox(bbox = c(-100, 37, -80, 52)) %>%
+  auk_bbox(bbox = c(-100, 37, -80, 52)) |>
   # date: use standard ISO date format `"YYYY-MM-DD"`
-  auk_date(date = c("2012-01-01", "2012-12-31")) %>%
+  auk_date(date = c("2012-01-01", "2012-12-31")) |>
   # time: 24h format
-  auk_time(start_time = c("06:00", "09:00")) %>%
+  auk_time(start_time = c("06:00", "09:00")) |>
   # duration: length in minutes of checklists
-  auk_duration(duration = c(0, 60)) %>%
+  auk_duration(duration = c(0, 60)) |>
   # complete: all species seen or heard are recorded
   auk_complete()
 ebd_filters
 
 ## ----auk-complete, eval = FALSE-----------------------------------------------
 # output_file <- "ebd_filtered_blja-grja.txt"
-# ebd_jays <- system.file("extdata/ebd-sample.txt", package = "auk") %>%
-#   auk_ebd() %>%
-#   auk_species(species = c("Canada Jay", "Cyanocitta cristata")) %>%
-#   auk_country(country = "Canada") %>%
+# ebd_jays <- system.file("extdata/ebd-sample.txt", package = "auk") |>
+#   auk_ebd() |>
+#   auk_species(species = c("Canada Jay", "Cyanocitta cristata")) |>
+#   auk_country(country = "Canada") |>
 #   auk_filter(file = output_file)
 
 ## ----read---------------------------------------------------------------------
-system.file("extdata/ebd-sample.txt", package = "auk") %>% 
-  read_ebd() %>% 
+system.file("extdata/ebd-sample.txt", package = "auk") |>
+  read_ebd() |>
   glimpse()
 
 ## ----read-auk-ebd, eval = FALSE-----------------------------------------------
 # output_file <- "ebd_filtered_blja-grja.txt"
-# ebd_df <- system.file("extdata/ebd-sample.txt", package = "auk") %>%
-#   auk_ebd() %>%
-#   auk_species(species = c("Canada Jay", "Cyanocitta cristata")) %>%
-#   auk_country(country = "Canada") %>%
-#   auk_filter(file = output_file) %>%
+# ebd_df <- system.file("extdata/ebd-sample.txt", package = "auk") |>
+#   auk_ebd() |>
+#   auk_species(species = c("Canada Jay", "Cyanocitta cristata")) |>
+#   auk_country(country = "Canada") |>
+#   auk_filter(file = output_file) |>
 #   read_ebd()
 
 ## ----awk-script---------------------------------------------------------------
-awk_script <- system.file("extdata/ebd-sample.txt", package = "auk") %>% 
-  auk_ebd() %>% 
-  auk_species(species = c("Canada Jay", "Cyanocitta cristata")) %>% 
-  auk_country(country = "Canada") %>% 
+awk_script <- system.file("extdata/ebd-sample.txt", package = "auk") |>
+  auk_ebd() |>
+  auk_species(species = c("Canada Jay", "Cyanocitta cristata")) |>
+  auk_country(country = "Canada") |>
   auk_filter(awk_file = "awk-script.txt", execute = FALSE)
 # read back in and prepare for printing
 awk_file <- readLines(awk_script)
 unlink("awk-script.txt")
-awk_file[!grepl("^[[:space:]]*$", awk_file)] %>% 
-  paste0(collapse = "\n") %>% 
+awk_file[!grepl("^[[:space:]]*$", awk_file)] |>
+  paste0(collapse = "\n") |>
   cat()
 
 ## ----auk-unique---------------------------------------------------------------
 # read in an ebd file and don't automatically remove duplicates
-ebd_dupes <- system.file("extdata/ebd-sample.txt", package = "auk") %>%
+ebd_dupes <- system.file("extdata/ebd-sample.txt", package = "auk") |>
   read_ebd(unique = FALSE)
 # remove duplicates
 ebd_unique <- auk_unique(ebd_dupes)
@@ -116,7 +116,7 @@ nrow(ebd_unique)
 
 ## ----auk-rollup---------------------------------------------------------------
 # read in sample data without rolling up
-ebd <- system.file("extdata/ebd-rollup-ex.txt", package = "auk") %>%
+ebd <- system.file("extdata/ebd-rollup-ex.txt", package = "auk") |>
   read_ebd(rollup = FALSE)
 # apply roll up
 ebd_ru <- auk_rollup(ebd)
@@ -128,22 +128,22 @@ unique(ebd_ru$category)
 
 # yellow-rump warbler subspecies rollup
 # without rollup, there are three observations
-ebd %>%
-  filter(common_name == "Yellow-rumped Warbler") %>%
+ebd |>
+  filter(common_name == "Yellow-rumped Warbler") |>
   select(checklist_id, category, common_name, subspecies_common_name,
          observation_count)
 # with rollup, they have been combined
-ebd_ru %>%
-  filter(common_name == "Yellow-rumped Warbler") %>%
+ebd_ru |>
+  filter(common_name == "Yellow-rumped Warbler") |>
   select(checklist_id, category, common_name, observation_count)
 
 ## ----ebd-zf-------------------------------------------------------------------
 # to produce zero-filled data, provide an EBD and sampling event data file
 f_ebd <- system.file("extdata/zerofill-ex_ebd.txt", package = "auk")
 f_smp <- system.file("extdata/zerofill-ex_sampling.txt", package = "auk")
-filters <- auk_ebd(f_ebd, file_sampling = f_smp) %>% 
-  auk_species("Collared Kingfisher") %>% 
-  auk_time(c("06:00", "10:00")) %>% 
+filters <- auk_ebd(f_ebd, file_sampling = f_smp) |>
+  auk_species("Collared Kingfisher") |>
+  auk_time(c("06:00", "10:00")) |>
   auk_complete()
 filters
 
